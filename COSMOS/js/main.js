b73e291415276bcfe146c2b784b64693fd369b73
olegnvetrov <65262104+olegnvetrov@users.noreply.github.com>
2020-06-29 21:37:01 +0300

    Add files via upload

--- COSMOS/js/main.js
+++ COSMOS/js/main.js
@@ -1,4 +1,14 @@
+$("body").on('click', '[href*="#"]', function(e){
+  var fixed_offset = 100;
+  $('html,body').stop().animate({ scrollTop: $(this.hash).offset().top - fixed_offset }, 1000);
+  e.preventDefault();
+});
 $(document).ready(function(){
+
+
+
+
+
 	$('.burger').click(
 
 		function(){
@@ -6,4 +16,59 @@ $(document).ready(function(){
 		$('.burger').toggleClass('active');
 	});
 
+ 	    $('.single-item2').slick({
+   infinite: true,
+      dots: true,
+      arrows:false,
+      slidesToShow: 3,
+      slidesToScroll: 1,
+      adaptiveHeight:true,
+       adaptiveWidth:true,
+         responsive: [
+    {
+      breakpoint: 767,
+      settings: {
+        slidesToShow: 1
+          
+      }
+    }
+
+
+  ]
+    });
+
+$('.show-map').click(function(){
+	$('#modal').removeClass("show-my");
+		$('#thanks').removeClass("show-my");
+	});
+	$(".show-map").click(function(e){
+		$('#modal').addClass("show-my");
+		$('#applyForm').addClass("course", $(this).attr('id'));
+	});
+$('#closeModal').click(function(e){
+   $('#modal').removeClass("show-my");
+
+ });
+
+
+$('#closeThanks').click(function(e){
+   $('#thanks').removeClass("show-my");
 });
+		
+
+
+
+
+
+
+
+
+});
+
+
+
+
+
+
+
+
